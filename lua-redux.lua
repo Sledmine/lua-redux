664ed45ac53e9d41a1053049685fe9da3452dfa0
Sledmine <40512223+Sledmine@users.noreply.github.com>
2022-04-04 21:27:20 -0600

    normalize code, add emmy lua annotations

--- lua-redux.lua
+++ lua-redux.lua
@@ -1,52 +1,58 @@
-local ActionTypes = {
-  INIT = "@@lua-redux/INIT"
-}
+local actionTypes = {INIT = "@@lua-redux/INIT"}
 
 local function inverse(table)
-  local newTable = { }
-  for k, v in pairs(table) do
-    newTable[v] = k
-  end
-  return newTable
+    local newTable = {}
+    for k, v in pairs(table) do
+        newTable[v] = k
+    end
+    return newTable
 end
 
+---@class reduxAction
+---@field type string
+---@field payload any
+
+---Create a redux store for the application state
+---@param reducer fun(action:string, payload:any)
+---@param preloadedState any
 local function createStore(reducer, preloadedState)
-  local store = {
-    reducer = reducer,
-    state = preloadedState,
-    subscribers = {}
-  }
+    local store = {reducer = reducer, state = preloadedState, subscribers = {}}
 
-  function store:subscribe(callback)
-    local i = table.insert(self.subscribers, callback)
-    return function()
-      table.remove(self.subscribers, inverse(self.subscribers)[callback])
+    ---Subscribe a function callback to this store
+    ---@param callback function
+    ---@return function unsubscribe Unsubscribe function for this store
+    function store:subscribe(callback)
+        local i = table.insert(self.subscribers, callback)
+        return function()
+            table.remove(self.subscribers, inverse(self.subscribers)[callback])
+        end
     end
-  end
-  function store:dispatch(action)
-    self.state = self.reducer(self.state, action)
-    for k, v in pairs(self.subscribers) do
-      v()
+
+    ---Dispatch an action event to the store
+    ---@param action reduxAction
+    function store:dispatch(action)
+        self.state = self.reducer(self.state, action)
+        for k, v in pairs(self.subscribers) do
+            v()
+        end
     end
-  end
-  function store:getState()
-    return self.state
-  end
-  function store:replaceReducer(reducer)
-    self.reducer = reducer
-    self:dispatch({
-      type = ActionTypes.INIT
-    })
-  end
 
-  store:dispatch({
-    type = ActionTypes.INIT
-  })
+    ---Return the state of the store
+    ---@return any
+    function store:getState()
+        return self.state
+    end
 
-  return store
+    ---Replace the reducer of this store with a given one
+    ---@return any
+    function store:replaceReducer(reducer)
+        self.reducer = reducer
+        self:dispatch({type = actionTypes.INIT})
+    end
+
+    store:dispatch({type = actionTypes.INIT})
+
+    return store
 end
 
-return {
-  ActionTypes = ActionTypes,
-  createStore = createStore
-}
+return {actionTypes = actionTypes, createStore = createStore}
